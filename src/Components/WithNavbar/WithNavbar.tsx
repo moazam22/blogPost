b84dc3068dff091f9e6c04cf7fba4fbad37c0cbb
moazam22 <moazamrana22@gmail.com>
2023-01-16 18:01:07 +0500

    removed un-used imports

--- src/Components/WithNavbar/WithNavbar.tsx
+++ src/Components/WithNavbar/WithNavbar.tsx
@@ -1,8 +1,6 @@
 import { ReactElement } from "react";
 import Header from "../Header/Header";
 import { MainContainer } from "../Styled/MainContainer.Styled";
-import { Flex } from "@chakra-ui/react";
-import Chat from "../Chat/Chat";
 
 interface Props{
   children: ReactElement;
